52f56b1250ead84b4d826f2c49f80363432b0d8d
ANewUserX <2776086057@qq.com>
2022-02-27 15:15:11 +0800

    mina

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.export = {
+  configurewebpack:{
+    resolve:{
+      alias:{
+        "assets":"@/assets",
+        "components":"@/components",
+        "common":"@/common",
+        "views":"@/views"
+      }
+    }
+  }
+}
